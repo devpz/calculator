7255a8dec9e518a09c25cd86be5641614867b410
Paweł Załęski <p.zaleski@samsung.com>
2023-10-20 21:47:20 +0200

    added parenthesis

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Pobieramy elementy kalkulatora
 const resultElement = document.getElementById("result");
 const buttons = document.querySelectorAll("button");
 
-let currentInput = ""; // Przechowuje aktualne wprowadzane liczby i operatory
-let currentOperator = ""; // Przechowuje aktualnie wybrany operator
+let currentInput = "";
+let currentOperator = "";
 
-// Funkcja obsługująca kliknięcie na przycisk
 function buttonClick(event) {
   const buttonValue = event.target.textContent;
 
   if (buttonValue >= "0" && buttonValue <= "9") {
-    // Kliknięto cyfrę, dodaj ją do aktualnego wprowadzenia
     currentInput += buttonValue;
   } else if (
     buttonValue === "+" ||
@@ -18,28 +15,33 @@ function buttonClick(event) {
     buttonValue === "*" ||
     buttonValue === "/"
   ) {
-    // Kliknięto operator, zapisz aktualne wprowadzenie i operator
     if (currentInput !== "") {
       currentOperator = buttonValue;
       currentInput += buttonValue;
     }
   } else if (buttonValue === "=") {
-    // Kliknięto "=" - oblicz wynik
     if (currentInput !== "") {
-      currentInput = eval(currentInput).toString();
+      currentInput = calculateExpression(currentInput);
       currentOperator = "";
     }
   } else if (buttonValue === "C") {
-    // Kliknięto "C" - wyczyść kalkulator
     currentInput = "";
     currentOperator = "";
+  } else if (buttonValue === "(" || buttonValue === ")") {
+    currentInput += buttonValue;
   }
 
-  // Wyświetl aktualne wprowadzenie w polu wynikowym
   resultElement.value = currentInput;
 }
 
-// Dodaj obsługę kliknięć dla wszystkich przycisków
+function calculateExpression(expression) {
+  try {
+    return eval(expression);
+  } catch (error) {
+    return "Błąd";
+  }
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", buttonClick);
 });
